b80feab045a7c47ee73b32d2659fd3fd211b818b
CTBlankenship <chthbl@protonmail.com>
2019-03-17 12:53:38 -0400

    Removed previously entered spaces

--- Memberships/Areas/Admin/Controllers/SectionController.cs
+++ Memberships/Areas/Admin/Controllers/SectionController.cs
@@ -70,8 +70,7 @@ namespace Memberships.Areas.Admin.Controllers
             }
             return View(section);
         }
-
-
+        
         // POST: Admin/Section/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
