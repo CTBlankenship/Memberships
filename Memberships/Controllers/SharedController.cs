e65d456b9dffb42d04334477ed5f2113ca2ed807
CTBlankenship <chthbl@protonmail.com>
2019-03-17 14:37:37 -0400

    Added Bootstrap formatting to Save and Back to List Button

---
+++ Memberships/Controllers/SharedController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Memberships.Controllers
+{
+    public class SharedController : Controller
+    {
+        public ActionResult Index()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
