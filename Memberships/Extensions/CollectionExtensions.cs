a62cb790b5cbe9d94e4cac8b36b434f40f097c5a
CTBlankenship <chthbl@protonmail.com>
2019-03-18 15:33:14 -0400

    Product Model added ProductLinkText and other properties

--- Memberships/Extensions/CollectionExtensions.cs
+++ Memberships/Extensions/CollectionExtensions.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace Memberships.Extensions
 {
-    public static class ICollectionExtensions
+    public static class CollectionExtensions
     {
         public static IEnumerable<SelectListItem>
             ToSelectListItem<T>(this ICollection<T> items, int selectedValue)
